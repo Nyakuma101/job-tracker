f44e9994b10d92d72354be42b2428cfdbd564c81
Nyakuma Wal <Nyakuma.wal@gmail.com>
2024-03-30 04:00:15 -0400

    Short Text feild created

--- src/Components/Common/ShortTextField/ShortTextField.jsx
+++ src/Components/Common/ShortTextField/ShortTextField.jsx
@@ -5,7 +5,7 @@ export default function ShortTextField() {
   return (
     <label className={styles.label}>
       <div className={styles.text}>Short Text Field</div>
-      <input placeholder={"Placeholder"} type="Text"  name="title__name"className={styles.form}></input>
+      <input placeholder={"Placeholder"} type="Text"  name="title__name"className={styles.input}></input>
     </label>
   );
 }
