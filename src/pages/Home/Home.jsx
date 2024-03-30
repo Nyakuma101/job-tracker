addf982ca3fa158cb7f8183bbb652d6d8274c680
Nyakuma Wal <Nyakuma.wal@gmail.com>
2024-03-30 14:19:42 -0400

    LongTextField: File set up JSX file: Input textarea form CSS:Created the css file with corresponding colors from figma
    ShortTextField: Changed css file
    CSS: Added padding bottom in the css file.
    JSX file: Added a div to wrap Input form and created width to 50% of the parent element

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Card from '../../components/Card/Card';
 import AddCardButton from '../../components/AddCardButton/AddCardButton';
 import ShortTextField from '../../components/ShortTextField/ShortTextField';
+import LongTextField from '../../components/LongTextField/LongTextField';
 
 export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
-      <ShortTextField text="Home Page"/>
+      <LongTextField text="Job Description"/>
+      <ShortTextField text="Job Title"/>
       <Card />
       <AddCardButton />
     </div>
