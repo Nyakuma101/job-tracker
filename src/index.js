1d544c35c46252ebcdc33c17c8c898bd87abb5f5
Wal Wal <hey@wal.dev>
2024-03-30 01:46:27 -0400

    Create page files for the pages we'll need

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Home from './pages/Home/Home';
 import reportWebVitals from './reportWebVitals';
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
+import ApplicationForm from './pages/ApplicationForm/ApplicationForm';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/add-application"
+        element={<ApplicationForm mode="create" />}
+      />
+      <Route path="/:id" element={<ApplicationForm mode="edit" />} />
+    </>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
