3464063cf79fd71d776bae1b7d6cb17884511e18
Nyakuma Wal <Nyakuma.wal@gmail.com>
2024-03-30 14:37:34 -0400

    LongTextField input changed to text Area

--- src/components/LongTextField/LongTextField.jsx
+++ src/components/LongTextField/LongTextField.jsx
@@ -6,7 +6,7 @@ export default function LongTextField({text}) {
     <label className={styles.label}>
       <div className={styles.container}>
       <div className={styles.text}>{text}</div>
-      <textarea placeholder={"Paste relevant information from the job  posting here."} type="text"  name="title__name"className={styles.input}></textarea>
+      <textarea placeholder={"Paste relevant information from the job  posting here."} type="text"  name="title__name"className={styles.textarea}></textarea>
       </div>
     </label>
   );
