13df64d22158eaeef05521f6ed14b32344b05c70
Nyakuma Wal <Nyakuma.wal@gmail.com>
2024-03-30 15:15:02 -0400

    Company : font change to rubic

--- src/components/ShortTextField/ShortTextField.jsx
+++ src/components/ShortTextField/ShortTextField.jsx
@@ -4,8 +4,10 @@ export default function ShortTextField({text}) {
 
   return (
     <label className={styles.label}>
+        <div className={styles.container}>
       <div className={styles.text}>{text}</div>
       <input placeholder={"Placeholder"} type="text"  name="title__name"className={styles.input}></input>
+      </div>
     </label>
   );
 }
