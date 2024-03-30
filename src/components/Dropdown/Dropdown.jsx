cc13bd737dc72338d58115c0eb82f735ad5a8f5c
Nyakuma Wal <Nyakuma.wal@gmail.com>
2024-03-30 17:06:52 -0400

    Created the option section with the dropdown Created color variable for the dropdown option in the index.css

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,12 +1,19 @@
 import styles from './Dropdown.module.css';
 
 export default function Dropdown({ text }) {
-    // const ["Not submitted"]
+
   return (
     <label className={styles.label}>
       <div className={styles.container}>
         <div className={styles.text}>{text}</div>
-        <select className={styles.select} ></select>      
+        <select className={styles.select}>
+          <option className={styles.NotSubmitted}>Not Submitted</option>
+          <option className={styles.Submitted}>Submitted</option>
+          <option className={styles.NoCallback}>No Callback</option>
+          <option className={styles.InterviewScheduled}>Interview Scheduled</option>
+          <option className={styles.Interviewing}>Interviewing</option>
+          <option className={styles.OfferReceived}>Offer Received</option>
+          </select>      
       </div>
     </label>
   );
