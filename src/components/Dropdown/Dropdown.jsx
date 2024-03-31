941cf303c9a1d8261dcac62d87bcec9b4f6b4787
Nyakuma Wal <Nyakuma.wal@gmail.com>
2024-03-30 20:39:36 -0400

    Creating a Dropdown that could be changed

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -3,13 +3,12 @@ import styles from './Dropdown.module.css';
 
 export default function Dropdown({ text }) {
   const [val, setVal] = useState('');
-  console.log(val);
   const status = ['Not Submitted', 'Submitted', 'No Callback', 'Interview Scheduled', 'Interviewing', 'Offer Received'];
-
   return (
     <label className={styles.label}>
       <div className={styles.container} />
       <div className={styles.text}>{text}</div>
+
       <select className={styles.select} onChange={(e) => setVal(e.target.value)}>
         {status.map((op) => (
           <option>{op}</option>
@@ -18,3 +17,27 @@ export default function Dropdown({ text }) {
     </label>
   );
 }
+
+//----keep good copy----
+
+// import { useState } from 'react';
+// import styles from './Dropdown.module.css';
+
+// export default function Dropdown({ text }) {
+//   const [val, setVal] = useState('');
+//   const status = ['Not Submitted', 'Submitted', 'No Callback', 'Interview Scheduled', 'Interviewing', 'Offer Received'];
+//   return (
+//     <label className={styles.label}>
+//       <div className={styles.container} />
+//       <div className={styles.text}>{text}</div>
+
+//       <select className={styles.select} onChange={(e) => setVal(e.target.value)}>
+//         {/* <option>{NotSubmitted}</option> */}
+
+//         {status.map((op) => (
+//           <option>{op}</option>
+//         ))}
+//       </select>
+//     </label>
+//   );
+// }
