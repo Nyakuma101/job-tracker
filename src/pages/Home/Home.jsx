746b8212e79eb32cfb99759e7f23ea7e25a4afec
Nyajal Wal <nyajal.w@gmail.com>
2024-04-16 00:11:21 -0400

    added hero and sections of home page. need to fix the height adn width of the cards. need to input logo in the top left

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import styles from './Home.module.css';
+import curvyDivider from '../../assets/images/curvy-divider-large.svg';
+import curvyDividerSmall from '../../assets/images/curvy-divider-small.svg';
 import Card from '../../components/Card/Card';
 import AddCardButton from '../../components/AddCardButton/AddCardButton';
 import ShortTextField from '../../components/ShortTextField/ShortTextField';
@@ -13,15 +16,30 @@ export default function Home() {
   const [priority, setPriority] = useState(1);
   return (
     <div>
-      <h1>Home page</h1>
-      <ShortTextField text="Short Text Field"/>
-      <LongTextField text="Long Text Field"/>
-      <Dropdown text="Dropdown"/>
+      <h1>Header/logo</h1>
+      <section className={styles.hero}>
+        <h1 className={styles.title}>14</h1>
+        <p className={styles.subtitle}>Active Applications</p>
+      </section>
+
+      <img className={styles.small__divider} src={curvyDividerSmall} alt="blue page divider" />
+      <img className={styles.divider} src={curvyDivider} alt="blue page divider" />
+
+      <main className={styles.main}>
+        <AddCardButton />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </main>
+      {/* <ShortTextField text="Short Text Field" />
+      <LongTextField text="Long Text Field" />
+      <Dropdown text="Dropdown" />
       <Card />
       <AddCardButton />
       <StarField priority={priority} setPriority={setPriority} />
       <SubmitButton text="Submit" />
-      <CheckBox label="Check Box" text="Check Box" />
+      <CheckBox label="Check Box" text="Check Box" /> */}
     </div>
   );
 }
